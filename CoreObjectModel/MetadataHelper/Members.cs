40a274aee4ffda84cef8bd9d26f1173100cbf130
walterpg <15221668+walterpg@users.noreply.github.com>
2024-06-06 18:57:14 -0500

    Fix generic method visibility.
    Generic method instance visibility (public, protected, etc.) is not
    constrained by its generic type arguments.

--- CoreObjectModel/MetadataHelper/Members.cs
+++ CoreObjectModel/MetadataHelper/Members.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -450,7 +450,7 @@ namespace Microsoft.Cci.Immutable {
     /// <value></value>
     public TypeMemberVisibility Visibility {
       get {
-        return TypeHelper.GenericInstanceVisibilityAsTypeMemberVisibility(this.GenericMethod.Visibility, this.GenericArguments);
+        return this.GenericMethod.Visibility;
       }
     }
 
